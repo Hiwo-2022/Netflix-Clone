9a238011fcb690df943412a7e9e60356cac8231a
Hiwo-2022 <dilacher78@gmail.com>
2025-05-14 14:41:54 -0400

    some css updates

--- Netflix-clone-1/src/App.jsx
+++ Netflix-clone-1/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-
-import './App.css'
 import Home from './Pages/Home/Home'
 
 function App() {
--- Netflix-clone-1/src/Components/Header/Header.jsx
+++ Netflix-clone-1/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Header.css";
 import NetflixLogo from "../../assets/Images/NetflixLogo.jpg";
 import SearchIcon from "@mui/icons-material/Search";
@@ -7,42 +7,79 @@ import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function Header() {
-  return (
-    <div className="header_outer_container">
-    <div className="header_container">
-      <div className="header_left">
-        <ul>
-          <li>
-            <img src={NetflixLogo} alt="Netflix Logo" width="100" />
-          </li>
-          <li>Netflix</li>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-          <li>My List</li>
-          <li>Browse by Languages</li>
-        </ul>
-      </div>
-      <div className="header_right">
-        <ul>
-          <li>
-            <SearchIcon />
-          </li>
-          <li>
-            <NotificationsNoneIcon />
-          </li>
-          <li>
-            <AccountBoxIcon />
-          </li>
-          <li>
-            <ArrowDropDownIcon />
-          </li>
-        </ul>
-      </div>
-    </div>
-    </div>
+  const headerRef = useRef(null);
+  const scrollTimeout = useRef();
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const banner = document.querySelector(".banner");
+      if (!banner || !headerRef.current) return;
+
+      const bannerRect = banner.getBoundingClientRect();
+
+      // If banner is visible
+      if (bannerRect.bottom > 0) {
+        // Show header background while scrolling
+        headerRef.current.classList.remove("transparent");
+        clearTimeout(scrollTimeout.current);
+        scrollTimeout.current = setTimeout(() => {
+          // If still in banner after scrolling stops, hide background
+          if (banner.getBoundingClientRect().bottom > 0) {
+            headerRef.current.classList.add("transparent");
+          }
+        }, 400); // 400ms after scrolling stops
+      } else {
+        // If banner is not visible, always show header background
+        headerRef.current.classList.remove("transparent");
+        clearTimeout(scrollTimeout.current);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    // Run once on mount
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearTimeout(scrollTimeout.current);
+    };
+  }, []);
+
+  return (
+    <div ref={headerRef} className="header_outer_container transparent">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
+            </li>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Games</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
