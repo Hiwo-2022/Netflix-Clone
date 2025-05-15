68e4d2c06cb3f547fed6edc258bd40b36a4a1a89
Hiwo-2022 <dilacher78@gmail.com>
2025-05-15 12:14:44 -0400

    depandancy fix

--- Netflix-clone-1/vite.config.js
+++ Netflix-clone-1/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/netflix-clone-1/", // Add this line
+  base: "/Netflix-Clone/", // <-- Change this to match your repo name exactly!
   plugins: [react()],
 });
